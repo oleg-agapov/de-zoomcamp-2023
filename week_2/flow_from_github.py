41b0d38c5091e7603cf1b326e09c960a71579949
Oleg Agapov <oleg.agapov.ua@gmail.com>
2023-02-03 19:57:54 +0100

    Rename flows

---
+++ week_2/flow_from_github.py
@@ -0,0 +1,14 @@
+from prefect.deployments import Deployment 
+from prefect.filesystems import GitHub
+from load_to_gcs import load_to_gcs
+
+github_block = GitHub.load("github-creds")
+
+github_deployment = Deployment.build_from_flow(
+    flow=load_to_gcs,
+    name="Load to GCS (github)",
+    path='week_2/load_to_gcs.py:load_to_gcs',
+    storage=github_block
+)
+
+github_deployment.apply()
